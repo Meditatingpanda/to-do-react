6d1545ec060797d0c02de0a35fd1df090ed2c986
Traveling Monk <grp.gyanaranajan@gmail.com>
2021-12-12 21:56:08 +0530

    make responsive

--- blog-post/src/listComponent.js
+++ blog-post/src/listComponent.js
@@ -9,8 +9,10 @@ const Blogcontent = ({blog,handleDelete}) => {
              {
             blog.map((key)=>{
                 return  <div className="content-box" key={key.id}>
-                      <span className="title"><span style={{color:'black'}}>Title:-</span>{key.title}</span>
-                      <button className="btn" onClick={()=>{handleDelete(key.id)}}>Delete</button>
+                       <div className="deleteBox">
+                        <span className="title"><span style={{color:'black'}}>Title:-</span>{key.title}</span>
+                        <button className="btn" onClick={()=>{handleDelete(key.id)}}>Delete</button>
+                       </div>
                       <p className="body">{key.body}</p>
                       <h6>Written by {key.author}</h6>
                   </div>
